abe296a2c0457d4e7bc991393990e7fb9a96db9a
psychome004 <anil.stephen66@gmail.com>
2024-06-18 15:31:27 +0530

    Add author template

---
+++ author.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * The template for displaying author page
+ */
+get_header();
+$author = $wp_query->get_queried_object();
+?>
+<div class="author-header archive-header">
+  <div class="container">
+    <h1 class="page-title text-capitalize text-center mmus-dec-af"><?php _e( $author->display_name ); ?></h1>
+    <div class="page-description text-center"><?php _e( $author->description ); ?></div>
+  </div>
+</div>
+<?php $shortcode_str = do_shortcode("[orbit_query posts_per_page='9' style='grid3-with-cat' author='".$author->ID."']"); ?>
+<?php if( strlen( $shortcode_str ) > 0 ): ?>
+  <div class="container">
+    <h3 class="text-center author-posts-headline">Articles by the author</h3>
+    <div class="articles-post-list-wrap">
+      <?php echo $shortcode_str; ?>
+    </div>
+  </div>
+<?php endif; ?>
+<?php get_footer(); ?>
