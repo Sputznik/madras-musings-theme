bef996e8dae7a6b2d2bbc8c93a5f3b8adaaab599
psychome004 <anil.stephen66@gmail.com>
2024-06-19 13:43:10 +0530

    Add download-pdf button in category template

--- category.php
+++ category.php
@@ -6,6 +6,7 @@ get_header();
 $category = $wp_query->get_queried_object();
 $grid_three_posts_count = 0;
 $grid_sticky_posts_count = 7;
+$pdf_url = get_term_meta( $category->term_id, 'mmus_category_pdf', true );
 
 // CALCULATE TOTAL POSTS TO BE SHOWN IN 3 COLUMN GRID
 if( $category->count > 7 ){
@@ -22,6 +23,11 @@ if( $category->count > 7 ){
   <div class="articles-post-list-wrap">
     <?php echo do_shortcode("[orbit_query posts_per_page='".$grid_sticky_posts_count."' style='grid-sticky' cat='".$category->term_id."']"); ?>
     <?php echo do_shortcode("[orbit_query offset='7' posts_per_page='".$grid_three_posts_count."' style='grid3' cat='".$category->term_id."']"); ?>
+    <?php if( $pdf_url ): ?>
+      <div class="download-pdf">
+        <a target="_blank" href="<?php _e( $pdf_url ); ?>">Download PDF</a>
+      </div>
+    <?php endif;?>
   </div>
 </div>
 <?php get_footer(); ?>
